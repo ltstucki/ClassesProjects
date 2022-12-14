c85a6994a990c88c3d0954838ee2b139932d83a9
ltstucki <inertiastooks@gmail.com>
2022-12-13 17:31:18 -0700

    tic tac toe update
    finished

--- tic_tac_toe_game/tic-tac-toe.cs
+++ tic_tac_toe_game/tic-tac-toe.cs
@@ -1,44 +1,38 @@
-﻿// Program: CSE 210 Tic Tac Toe Example
-// Author: Brother Burton
-
-using System;
+﻿using System;
 using System.Collections.Generic;
-
-/// <summary>
-/// This is a sample solution of the Tic Tac Toe program. It is not a
-/// particularly sophisticated or elegant solution to the problem, but
-/// rather it highlights a straightforward way to solve the problem
-/// for a 3x3 game.
-/// </summary>
 class Program
+// Performed in class as group project.
+// Commented and shown understanding by Landon Stucki.
 {
     static void Main(string[] args)
     {
+        // Checks for the current player.
         List<string> board = GetNewBoard();
         string currentPlayer = "x";
 
+        // For as long as the game is NOT over, keep running until IsGameOver bool is True.
         while (!IsGameOver(board))
+        // IF BOOL IS TRUE, END GAME AND DONT RUN ANYTHING IN THE LOOP.
         {
+            // Display the board
             DisplayBoard(board);
-
+            // Grabs the players choice of 1-9.
             int choice = GetMoveChoice(currentPlayer);
+            // Checks the current player (x or o) and puts his choice (1-9) on the board.
             MakeMove(board, choice, currentPlayer);
-
+            // Gives the next player in line (x or o) priority for next turn.
             currentPlayer = GetNextPlayer(currentPlayer);
         }
 
+        // As soon as the game is over and the boolean is set to true, display the board and give gameover message.
         DisplayBoard(board);
         Console.WriteLine("Good game. Thanks for playing!");
     }
 
-    /// <summary>
-    /// Gets a new instance of the board with the numbers 1-9 in place.
-    /// </summary>
-    /// <returns>A list of 9 strings representing each square.</returns>
     static List<string> GetNewBoard()
     {
         List<string> board = new List<string>();
-
+        // Creates a list of 9 numbers to be used on the 3x3 board.
         for (int i = 1; i <= 9; i++)
         {
             board.Add(i.ToString());
@@ -47,50 +41,35 @@ class Program
         return board;
     }
 
-    /// <summary>
-    /// Displays the board in a 3x3 grid.
-    /// </summary>
-    /// <param name="board">The board</param>
     static void DisplayBoard(List<string> board)
     {
-        // This could be done more elegantly using loops and if statements
-        // especially if something besides 3x3 was ever anticipated.
+        // Displays the boards numbers in a nice 3x3 grid as so...
+        // 1|2|3
+        // -+-+-
+        // 4|5|6
+        // -+-+-
+        // 7|8|9
         Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
         Console.WriteLine("-+-+-");
         Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
         Console.WriteLine("-+-+-");
         Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
     }
-
-    /// <summary>
-    /// Determines if the game is over because of a win or a tie.
-    /// </summary>
-    /// <param name="board">The current board.</param>
-    /// <returns>True if the game is over</returns>
     static bool IsGameOver(List<string> board)
     {
         bool isGameOver = false;
 
         if (IsWinner(board, "x") || IsWinner(board, "o") || IsTie(board))
         {
+            // Checks if the game is over, if so, set bool to true.
             isGameOver = true;
         }
 
         return isGameOver;
     }
-
-    /// <summary>
-    /// Determines if the provided player has a tic tac toe.
-    /// </summary>
-    /// <param name="board">The current board</param>
-    /// <param name="player">The player to check for a win</param>
-    /// <returns></returns>
     static bool IsWinner(List<string> board, string player)
     {
-        // There are more elegant ways to check for a win, especially if
-        // something besides a 3x3 board were anticipated. This brute force
-        // approach is sufficient to check for the possibilities.
-
+        // Every possible way to win is displayed here in an if statement.
         bool isWinner = false;
 
         if ((board[0] == player && board[1] == player && board[2] == player)
@@ -103,25 +82,21 @@ class Program
             || (board[2] == player && board[4] == player && board[6] == player)
             )
         {
+            // If one of these 3 indexes line up, set isWinner bool to true.
             isWinner = true;
         }
 
         return isWinner; 
     }
-
-    /// <summary>
-    /// Determines if the board is full with no more moves possible.
-    /// </summary>
-    /// <param name="board">The current board.</param>
-    /// <returns>True if the board is full.</returns>
     static bool IsTie(List<string> board)
     {
-        // If there is a digit, there are still moves to be made.
         bool foundDigit = false;
 
         foreach (string value in board)
         {
             if (char.IsDigit(value[0]))
+            // Checks if there are any more spaces on the board to make a move.
+            // If there are not, it must be a tie.
             {
                 foundDigit = true;
                 break;
@@ -130,14 +105,9 @@ class Program
 
         return !foundDigit;
     }
-
-    /// <summary>
-    /// Cycles through the players (from x to o and o to x)
-    /// </summary>
-    /// <param name="currentPlayer">The current players sign (x or o)</param>
-    /// <returns>The next players sign (x or o)</returns>
     static string GetNextPlayer(string currentPlayer)
     {
+        // Goes back and forth between X turn and O turn, starting with X.
         string nextPlayer = "x";
 
         if (currentPlayer == "x")
@@ -148,13 +118,9 @@ class Program
         return nextPlayer;
     }
 
-    /// <summary>
-    /// Gets the 1-based spot number associated with the user's choice.
-    /// </summary>
-    /// <param name="currentPlayer">The sign (x or o) of the current player.</param>
-    /// <returns>A 1-based spot number (not a 0-based index)</returns>
     static int GetMoveChoice(string currentPlayer)
     {
+        // Gets the players choice of placement on the board (1-9).
         Console.Write($"{currentPlayer}'s turn to choose a square (1-9): ");
         string? move_string = Console.ReadLine();
 
@@ -166,20 +132,12 @@ class Program
         return choice;
     }
 
-    /// <summary>
-    /// Places the current players mark on the board at the desired spot.
-    /// This method does NOT check to ensure the spot is available.
-    /// </summary>
-    /// <param name="board">The current board</param>
-    /// <param name="choice">The 1-based spot number (not a 0-based index).</param>
-    /// <param name="currentPlayer">The current player's sign (x or o)</param>
     static void MakeMove(List<string> board, int choice, string currentPlayer)
+    // Grabs the players mark (x or o) and the spot chosen on the board (1-9)
+    // Puts that mark in chosen spot.
     {
-        // Convert the 1-based spot number to a 0-based index.
         int index = choice - 1;
 
-        // It would be good to include an additional check here to ensure that
-        // the spot is available.
         board[index] = currentPlayer;
     }
 }
